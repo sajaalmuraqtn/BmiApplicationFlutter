368db90d3b2abf847f5124529fe576f32f5d0dbe
sajaalmuraqtn <sajaalmuraqtn@gmail.com>
2025-09-01 05:12:27 +0300

    Application Structure

---
+++ lib/helpers/routes.dart
@@ -0,0 +1,8 @@
+class AppRoutes {
+  static const String splash = '/'; 
+
+  static const String login = '/login';
+  static const String signup = '/signup';
+  static const String services = '/services';
+  static const String dash = '/dash';
+} 
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:bmi_app/helpers/routes.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Text("hello to bmi app"),
+    // routes:{
+    //   AppRoutes.splash:(context)=>
+    // } ,
+  ) );
+}
+ 
